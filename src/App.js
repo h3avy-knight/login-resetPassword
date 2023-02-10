14960846bcff0373ff7a68bea05f21fd3b62bc42
h3avy-knight <rohityadavsiri@gmail.com>
2023-02-11 00:47:21 +0530

    Tailwind css added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 const App = () => {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 };
 
 export default App;
